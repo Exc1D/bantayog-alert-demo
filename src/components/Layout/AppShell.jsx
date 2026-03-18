939ecd02171e23d38521793f9fd782752cbd2d83
David Laurence Aviado <davidaviado.dla@gmail.com>
2026-03-18 10:43:27 +0800

    feat: add AppShell layout route component

---
+++ src/components/Layout/AppShell.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import TabNavigation from './TabNavigation';
+
+export default function AppShell() {
+  return (
+    <div className="flex flex-col h-dvh bg-app-bg overflow-hidden">
+      <Header />
+      <main className="flex-1 overflow-hidden relative">
+        <Outlet />
+      </main>
+      <TabNavigation />
+    </div>
+  );
+}
